3d7fb7c09ef55ccb6cb94b781a651addfbd2a231
NicolasCuljkar <yourgculjkarn@hotmail.fr>
2022-05-16 09:12:42 +0200

    init

---
+++ src/GameManager.js
@@ -0,0 +1,8 @@
+export class GameManager{
+    constructor(positionScore) {
+        this.nbVies = 3;
+        this.score = 0;
+        this.positionScore = positionScore;
+        this.gameState = "";
+    }
+}
